9c7421f3821d66ae70f193e13ca752107c9fac96
marb <marb61a@gmail.com>
2015-08-23 08:41:35 +0000

    updated routes and layout files

---
+++ client/templates/layout.js
@@ -0,0 +1,6 @@
+// Truncates text
+Template.registerHelper('truncateText', function(text, length){
+	var newText = text.substring(0, length);
+	newText = newText.substr(0, Math.min(newText.length, newText.lastIndexOf(" ")));
+	return new Spacebars.SafeString(newText)
+});
