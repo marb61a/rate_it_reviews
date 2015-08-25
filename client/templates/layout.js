9ecaee9e844adb667511235937c4e84b83d8863a
marb <marb61a@gmail.com>
2015-08-25 08:10:26 +0000

    updated route and layout files

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -4,3 +4,22 @@ Template.registerHelper('truncateText', function(text, length){
 	newText = newText.substr(0, Math.min(newText.length, newText.lastIndexOf(" ")));
 	return new Spacebars.SafeString(newText)
 });
+
+// Get average rating
+Template.registerHelper('getAvg', function(reviews){
+	var sum = 0;
+	for (var i = 0; i < reviews.length; i++) {
+        sum += parseInt(reviews[i].rating, 10);
+    }
+    var avg = sum / reviews.length;
+    return Math.round(avg);
+});
+
+// Get reviews total
+Template.registerHelper('getReviewsTotal', function(total){
+	if(total > 0){
+    	return total;
+   	} else {
+    	return 0;
+   	}
+});
